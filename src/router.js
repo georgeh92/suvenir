3ba3b7a5b2d86c00f681565b3fa5e823fc47e83c
design <C:\Users\User\AppData\Roaming\The Bat!>
2019-07-29 17:53:31 +0300

    Создание компонента Pressa

--- src/router.js
+++ src/router.js
@@ -28,6 +28,14 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('./views/Eleqance.vue')
+    },
+    {
+      path: '/Pressa',
+      name: 'Pressa',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('./views/Pressa.vue')
     }
   ]
 })
